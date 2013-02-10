c0222fa4fb9e16174007681cc1c0f578514fddaf
chi-feng <email.chi.feng@gmail.com>
2013-02-10 02:06:34 -0500

    Updated article auto-populate function

--- includes/admin_footer.php
+++ includes/admin_footer.php
@@ -11,10 +11,9 @@ $(document).ready(function() {
   var article_auto_populate = '<div id="auto-populate" class="clearfix">\
   <p>Auto-populate from entry: Author, A.B., Author, C.D. "Title." Journal Name: Volume Pages (Year)</p>\
   <textarea class="auto-populate"></textarea><br />\
-  <input type="button" class="btn" id="auto-populate-button" value="Parse" />\
   </div>';
   $('form.create-article fieldset').prepend(article_auto_populate);
-  $('#auto-populate-button').click(function() {
+  $('textarea.auto-populate').keyup(function() {
     parse_article($('textarea.auto-populate').val());
   });
 });
