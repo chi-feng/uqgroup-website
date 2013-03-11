65248aeab436ae46014274eb6ba738ad4b67a89d
Chi Feng <email.chi.feng@gmail.com>
2013-03-11 03:39:07 -0300

    Update people.php

--- pages/people.php
+++ pages/people.php
@@ -28,15 +28,18 @@
       <li><a href="profiles#tosatto">Luca Tosatto</a></li>
     </ul>
     
-    <h3>Visiting Associates</h3>
+    <h3>Current Visitors</h3>
     <ul>
-      <li><a href="profiles#stavropoulou">Faidra Stavropoulou</a></li>
+    
+  <li><a href="http://perso.limsi.fr/Individu/mathelin/">Lionel Mathelin, LIMSI/CNRS (France)</a></li>
+  <li><a href="http://www.ucl.ac.uk/statistics/people/bencalderhead">Ben Calderhead, University College London (UK)</a></li>
+  <li><a href="http://www.jinglaili.net/">Jinglai Li, Shanghai Jiatong University (China)</a></li>
+    
     </ul>
     
-    <h3>Undergraduates, SB &amp; UROP</h3>
+    <h3>Undergraduate Students (UROP)</h3>
     <ul>
-      <li><a>George Hansel (MIT physics)</a></li>
-      <li><a>Michael Lieu (MIT Aero Astro)</a></li>
+      <li><a>Yair Shenfeld</a></li>
     </ul>
     
   </div>
@@ -45,6 +48,7 @@
     <h3>Graduate Students, PhD</h3>
     <ul>
       <li><a href="profiles#conrad">Patrick Conrad</a></li>
+      <li><a href="profiles#davis">Andrew Davis</a></li>
       <li><a href="profiles#galagali">Nikhil Galagali</a></li>
       <li><a href="profiles#gorodetsky">Alex Gorodetsky</a></li>
       <li><a href="profiles#huan">Xun Huan</a></li>
@@ -57,32 +61,39 @@
     <ul>
       <li><a href="profiles#aggarwal">Raghav Aggarwal</a></li>
       <li><a href="profiles#feng">Chi Feng</a></li>
-      <li><a href="profiles#mitra">Subhadeep Mitra</a></li>
     </ul>
     
   </div>
 </div>
-
 <br style="clear:both" />
 
 <h2>Alumni</h2>
 <div class="alumni">
   <h3>Postdoctoral Alumni</h3>
   <ul class="list">
-    <li><strong>Ingrid Berkelmans</strong> (Cambridge Chemical)</li>
-    <li><strong>Sonjoy Das</strong> (University of Buffalo, Mechanical and Aerospace Engineering)</li>
+    <li><strong>Ingrid Berkelmans</strong> (Cambridge Chemical Technologies)</li>
+    <li><strong>Sonjoy Das</strong> (University of Buffalo, Mechanical and Aerospace Engineering) <a href="http://lotus.eng.buffalo.edu/"><i class="icon-external-link"></i></a></li>
     <li><strong>Michalis Frangos</strong> (Schlumberger)</li>
-    <li><strong>Jinglai Li</strong> (Shanghai Jiaotong University) <a href="http://mit.edu/jinglai/www/web/home.html"><i class="icon-external-link">&nbsp;</i></a></li>
+    <li><strong>Jinglai Li</strong> (Shanghai Jiaotong University) <a href="http://www.jinglaili.net/"><i class="icon-external-link">&nbsp;</i></a></li>
     <li><strong>Ankur Srivastava</strong> (GE Global Research) <a href="http://www.linkedin.com/pub/ankur-srivastava/21/285/6b6"><i class="icon-external-link">&nbsp;</i></a></li>
   </ul>
   <h3>Graduate Alumni</h3>
   <ul class="list">
-    <li><strong>Thomas Coles</strong> (MIT)</li>
+    <li><strong>Thomas Coles</strong> (MIT)</strong></li>
+      <li><strong>Thomas Coles (MIT)</strong></li>
+      <li><strong>Subhadeep Mitra (Citibank)</strong></li>
+  </ul>
+  <h3>Long Term Visitors</h3>
+  <ul class="list">
+    <li><strong>Faidra Stavropoulou</strong></li>
   </ul>
   <h3>Undergraduate Alumni</h3>
   <ul class="list">
     <li><strong>Tomas Kogan</strong> (Cambridge University)</li>
     <li><strong>Kevin Lim</strong> (Princeton University, Department of Economics)</li>
     <li><strong>Hadi Kasab</strong> (American University of Beirut)</li>
+    <li><strong>George Hansel (MIT)</strong></li>
+    <li><strong>Michael Lieu (MIT)</strong></li>
+    <li><strong>Savithru Jayasinghe (Cambridge University)</strong></li>
   </ul>
 </div>
