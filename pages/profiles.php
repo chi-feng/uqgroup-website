66626f7b57c262f973617aced3f6e0594ac6d874
Chi Feng <email.chi.feng@gmail.com>
2013-03-11 23:32:06 -0300

    Update profiles.php

--- pages/profiles.php
+++ pages/profiles.php
@@ -13,7 +13,15 @@
 <div id="marzouk" class="person clearfix">
   <div class="photo"><img src="images/people/marzouk.gif" alt="marzouk" /></div>
   <div class="name"><a href="http://web.mit.edu/aeroastro/people/marzouk.html">Youssef Marzouk</a></div>
-  <div class="bio">He is the Class of 1942 Associate Professor of Aeronautics and Astronautics at MIT. He received his SB (1997), SM (1999), and PhD (2004) degrees in mechanical engineering from MIT. Afterwards, he spent four years at Sandia National Laboratories in Livermore, California—first as a Truman postdoctoral fellow and then as a senior member of the technical staff—before returning to MIT and joining the faculty in 2009. He is a recipient of the Hertz Foundation Doctoral Thesis Prize (2004) and the DOE Early Career Research Award (2010). Youssef is also an avid classical pianist.</div>
+  <div class="bio">
+    He is the Class of 1942 Associate Professor of Aeronautics and Astronautics at MIT. 
+    He received his SB (1997), SM (1999), and PhD (2004) degrees in mechanical engineering from MIT. 
+    Afterwards, he spent four years at Sandia National Laboratories in Livermore, California — 
+    first as a Truman postdoctoral fellow and then as a senior member of the technical staff — 
+    before returning to MIT and joining the faculty in 2009. He is a recipient of the Hertz Foundation 
+    Doctoral Thesis Prize (2004) and the DOE Early Career Research Award (2010). Youssef is also an avid 
+    classical pianist.
+  </div>
 </div>
 
 <h3 id="postdocs">Postdoctoral Associates</h3>
@@ -24,7 +32,11 @@
     <a href="">Florian Augustin</a>
   </div>
   <div class="bio">
-  I am interested in uncertainty quantification with Polynomial Chaos. My research includes the convergence analysis of numerical algorithms based on the stochastic Galerkin approach as well as structure preserving transformations for the Galerkin projected system. Moreover, I am interested in Compressive Sensing and robust optimization based on derivative free methods. I received my doctoral degree in Mathematics from the TUM (Munich, Germany) in 2011.
+    I am interested in uncertainty quantification with Polynomial Chaos. My research includes the 
+    convergence analysis of numerical algorithms based on the stochastic Galerkin approach as well as 
+    structure preserving transformations for the Galerkin projected system. Moreover, I am interested 
+    in Compressive Sensing and robust optimization based on derivative free methods. I received my 
+    doctoral degree in Mathematics from the TUM (Munich, Germany) in 2011.
   </div>
 </div>
 
@@ -34,6 +46,7 @@
     <a href="">Tiangang Cui</a>
   </div>
   <div class="bio">
+    Stuff.
   </div>
 </div>
 
@@ -42,7 +55,13 @@
   <div class="name">
     <a href="http://web.mit.edu/tmoselhy/www/">Tarek El Moselhy</a>
   </div>
-  <div class="bio">My research interests include numerical methods for stochastic PDEs, Bayesian inference, interconnect parasitic extraction under manufacturing uncertainties, and floating random walk. I received my PhD in electrical engineering from MIT in 2010, working with Prof. Luca Daniel in the Computational Prototyping Group. I received my MSc in engineering mathematics (2005) a Diploma in mathematics (2003) and my BSc in communication engineering (2000) all from Cairo University, Egypt.</div>
+  <div class="bio">
+    My research interests include numerical methods for stochastic PDEs, Bayesian inference, 
+    interconnect parasitic extraction under manufacturing uncertainties, and floating random walk. I received 
+    my PhD in electrical engineering from MIT in 2010, working with Prof. Luca Daniel in the Computational 
+    Prototyping Group. I received my MSc in engineering mathematics (2005) a Diploma in mathematics (2003) and 
+    my BSc in communication engineering (2000) all from Cairo University, Egypt.
+    </div>
 </div>
 
 <div id="tosatto" class="person clearfix">
@@ -59,7 +78,12 @@
   <div class="name">
     <a href="http://web.mit.edu/">Patrick Conrad</a>
   </div>
-  <div class="bio">I am interested in solving inverse problems with polynomial chaos, which starts by creating effective approximations. Second, I am interested in finding ways of performing Bayesian inference more effectively, by developing Markov chain Monte Carlo algorithms that exploit problem structure. When not researching, I enjoy sailing and ultimate frisbee.</div>
+  <div class="bio">
+    I am interested in solving inverse problems with polynomial chaos, which starts by creating 
+    effective approximations. Second, I am interested in finding ways of performing Bayesian inference more 
+    effectively, by developing Markov chain Monte Carlo algorithms that exploit problem structure. When not 
+    researching, I enjoy sailing and ultimate frisbee.
+  </div>
 </div>
 
 <div id="davis" class="person clearfix">
@@ -77,7 +101,11 @@
     <a href="http://web.mit.edu/">Nikhil Galagali</a>
   </div>
   <div class="bio">
-    My general research interests include Bayesian inference, Monte Carlo methods, and their applications to problems in chemical kinetics. Specifically, my current work focuses on the development of new model selection approaches for the elucidation of chemical reaction mechanisms. Prior to joining the UQ group, I received an SM degree from MIT and a B.Tech. degree from IIT Madras. Away from the lab, I enjoy music, playing squash, and running. 
+    My general research interests include Bayesian inference, Monte Carlo methods, and their applications 
+    to problems in chemical kinetics. Specifically, my current work focuses on the development of new model 
+    selection approaches for the elucidation of chemical reaction mechanisms. Prior to joining the UQ group, 
+    I received an SM degree from MIT and a B.Tech. degree from IIT Madras. Away from the lab, I enjoy music, 
+    playing squash, and running. 
   </div>
 </div>
     
@@ -86,7 +114,12 @@
   <div class="name">
     <a href="">Alex Gorodetsky</a>
   </div>
-  <div class="bio">I am interested in function approximation, statistical inference, numerical methods, and fluid mechanics. I am currently working on discontinuity detection and function approximation for computational models. I grew up in Chicago and received my BSE in aerospace engineering from the University of Michigan. I am an avid sports fan and enjoy playing basketball.</div>
+  <div class="bio">
+    I am interested in function approximation, statistical inference, numerical methods, and fluid mechanics. 
+    I am currently working on discontinuity detection and function approximation for computational models. 
+    I grew up in Chicago and received my BSE in aerospace engineering from the University of Michigan. 
+    I am an avid sports fan and enjoy playing basketball.
+  </div>
 </div>
 
 <div id="huan" class="person clearfix">
@@ -94,7 +127,12 @@
   <div class="name">
     <a href="http://web.mit.edu/xunhuan/www/">Xun Huan</a>
   </div>
-  <div class="bio">I am interested in uncertainty quantification, experimental design, Bayesian inference, model reduction, computational fluid dynamics, numerical methods and optimization. I love playing sports, especially ice hockey, ice skating, and pretty much any activity that takes place on ice! I am also interested in business law and entrepreneurship.</div>
+  <div class="bio">
+    I am interested in uncertainty quantification, experimental design, Bayesian inference, model reduction, 
+    computational fluid dynamics, numerical methods and optimization. I love playing sports, especially ice 
+    hockey, ice skating, and pretty much any activity that takes place on ice! I am also interested in business 
+    law and entrepreneurship.
+  </div>
 </div>
     
 <div id="naveen" class="person clearfix">
@@ -102,7 +140,11 @@
   <div class="name">
     <a href="http://web.mit.edu/nkartik/www">Naveen Kartik Conjeevaram Krishnakumar</a>
   </div>
-  <div class="bio">My research interests lie in the area of Bayesian computation and model selection. I graduated from the Chemical Engineering Department at IIT Madras with a Bachelors of Technology in Chemical Engineering and a minor in Material Science in July 2011.</div>
+  <div class="bio">
+    My research interests lie in the area of Bayesian computation and model selection. I graduated from the 
+    Chemical Engineering Department at IIT Madras with a Bachelors of Technology in Chemical Engineering 
+    and a minor in Material Science in July 2011.
+  </div>
 </div>
 
 <div id="parno" class="person clearfix">
@@ -110,7 +152,13 @@
   <div class="name">
     <a href="http://web.mit.edu/~mparno/www/">Matthew Parno</a>
   </div>
-  <div class="bio">My work focuses on multiscale Bayesian inference for large scale problems arising in hydrology. Growing up I briefly lived in Oregon and New York but spent most of the time in Minnesota where I finished my last two years of high school at the University of Minnesota.  After high school I worked as a raft guide before heading to Clarkson University where I balanced getting two degrees with whitewater kayaking and skiing.</div>
+  <div class="bio">
+    My work focuses on multiscale Bayesian inference for large scale problems arising in hydrology. Growing 
+    up I briefly lived in Oregon and New York but spent most of the time in Minnesota where I finished my 
+    last two years of high school at the University of Minnesota.  After high school I worked as a raft 
+    guide before heading to Clarkson University where I balanced getting two degrees with whitewater 
+    kayaking and skiing.
+  </div>
 </div>
     
 <div id="spantini" class="person clearfix">
@@ -118,7 +166,13 @@
   <div class="name">
     <a href="">Alessio Spantini</a>
   </div>
-  <div class="bio">My current research focuses in developing methods to enforce and efficiently exploit a low rank structure in the solution of a stochastic PDE via measure transformations (space-time stretching). Broadly speaking, I am interested in linear algebra, optimization, model reduction and UQ. I grew up in Italy and I graduated in Aerospace Engineering from Politecnico di Milano in July, 2011. In my free time I love sailing and jogging. I also hold a Private Pilot License.</div>
+  <div class="bio">
+    My current research focuses in developing methods to enforce and efficiently exploit a low rank structure 
+    in the solution of a stochastic PDE via measure transformations (space-time stretching). Broadly speaking, 
+    I am interested in linear algebra, optimization, model reduction and UQ. I grew up in Italy and I graduated 
+    in Aerospace Engineering from Politecnico di Milano in July, 2011. In my free time I love sailing and jogging. 
+    I also hold a Private Pilot License.
+  </div>
 </div>
     
 <h3 id="students-sm">Graduate Students, SM</h3>
@@ -128,7 +182,9 @@
   <div class="name">
     <a href="">Raghav Aggarwal (co-advised with Michael Demkowicz, MIT DMSE)</a>
   </div>
-  <div class="bio">Studying the underlying models which describe the nature of materials under extreme stress and strain.</div>
+  <div class="bio">
+    Studying the underlying models which describe the nature of materials under extreme stress and strain.
+  </div>
 </div>
 
 <div id="feng" class="person clearfix">
@@ -137,8 +193,12 @@
     <a href="http://chifeng.scripts.mit.edu">Chi Feng</a>
   </div>
   <div class="bio">
-My research aims to extend the Bayesian optimal design framework to cases where the predictive model is incomplete or incorrect. I am seeking my master's degree in the Computation for Design and Optimization program. I received a bachelor's degree in Physics from the California Institute of Technology, where I did a SURF in the Caltech PSAAP program. Outside of my work, I am interested in photography, piano, and web design. 
-</div>
+    My research aims to extend the Bayesian optimal design framework to cases where the predictive model is 
+    incomplete or incorrect. I am seeking my master's degree in the Computation for Design and Optimization 
+    program. I received a bachelor's degree in Physics from the California Institute of Technology, where I 
+    did a SURF in the Caltech PSAAP program. Outside of my work, I am interested in photography, piano, 
+    and web design. 
+  </div>
 </div>
 
 
