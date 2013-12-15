ecde485051ca1c812230c9a62e247e2cac3ea7f4
Chi Feng <chifeng@mit.edu>
2013-12-15 15:20:58 -0500

    Massaged outings layout

--- pages/outings.php
+++ pages/outings.php
@@ -1,14 +1,16 @@
 <?php if (!defined('INCLUDE_GUARD')) { header("HTTP/1.0 403 Forbidden"); die(); } ?>
 
 <div class="outing">
-  <h3>2013 Winter Brunch</h3>
+  <h3>Brunch at The Blue Room (2013/12)</h3>
   <div class="gallery">
     <a href="/images/outings/2013brunch/IMG_2406.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2406.jpg" /></a>
     <a href="/images/outings/2013brunch/IMG_2407.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2407.jpg" /></a>
     <a href="/images/outings/2013brunch/IMG_2425.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2425.jpg" /></a>
     <a href="/images/outings/2013brunch/IMG_2439.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2439.jpg" /></a>
-    <a href="/images/outings/2013brunch/IMG_2447.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2447.jpg" /></a>
+    <!-- <a href="/images/outings/2013brunch/IMG_2447.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2447.jpg" /></a> -->
     <a href="/images/outings/2013brunch/IMG_2457.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2457.jpg" /></a>
     <a href="/images/outings/2013brunch/IMG_2459.jpg" data-lightbox="2013brunch"><img src="images/outings/2013brunch/thumb.IMG_2459.jpg" /></a>
   </div>
 </div>
+
+<br style="clear: both" />
