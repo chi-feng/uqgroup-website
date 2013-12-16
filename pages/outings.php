63ccce24d25335224516f927ea556686165e406f
Chi Feng <chifeng@mit.edu>
2013-12-15 22:38:08 -0500

    Added Lionel-going-away-party to Outings

--- pages/outings.php
+++ pages/outings.php
@@ -1,5 +1,7 @@
 <?php if (!defined('INCLUDE_GUARD')) { header("HTTP/1.0 403 Forbidden"); die(); } ?>
 
+<h2>Group Outings</h2>
+
 <div class="outing">
   <h3>Brunch at The Blue Room (2013/12)</h3>
   <div class="gallery">
@@ -16,11 +18,22 @@
 <br style="clear: both" />
 
 <div class="outing">
-  <h3>Apple Picking (2013/10)</h3>
+  <h3>Apple picking (2013/10)</h3>
   <div class="gallery">
     <a href="/images/outings/2013apple/apple1.jpg" data-lightbox="2013apple"><img src="images/outings/2013apple/thumb.apple1.jpg" /></a>
     <a href="/images/outings/2013apple/apple2.jpg" data-lightbox="2013apple"><img src="images/outings/2013apple/thumb.apple2.jpg" /></a>
     <a href="/images/outings/2013apple/apple3.jpg" data-lightbox="2013apple"><img src="images/outings/2013apple/thumb.apple3.jpg" /></a>
+  </div>
+</div>
+
+<br style="clear: both" />
+
+<div class="outing">
+  <h3>Going-away dinner for Lionel (2013/05)</h3>
+  <div class="gallery">
+    <a href="/images/outings/2013lionel/IMG_4684.jpg" data-lightbox="2013lionel"><img src="images/outings/2013lionel/thumb.IMG_4684.jpg" /></a>
+    <a href="/images/outings/2013lionel/IMG_4686.jpg" data-lightbox="2013lionel"><img src="images/outings/2013lionel/thumb.IMG_4686.jpg" /></a>
+    <a href="/images/outings/2013lionel/IMG_4703.jpg" data-lightbox="2013lionel"><img src="images/outings/2013lionel/thumb.IMG_4703.jpg" /></a>
   </div>
 </div>
 
