5daeea9204deef7421dc136491d15976c48216b2
Chi Feng <chifeng@mit.edu>
2014-01-09 04:46:22 -0500

    removed InstantClick

--- includes/template.php
+++ includes/template.php
@@ -150,7 +150,5 @@ _gaq.push(['_trackPageview']);
   var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
 </script>
-<script src="/js/instantclick.js" data-no-instant></script>
-<script>InstantClick.init();</script>
 </body>
 </html>
