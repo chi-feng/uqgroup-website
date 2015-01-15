8ffe28ecbccfb8b2b2f3097374a829c9d403e2ba
Chi Feng <chifeng@mit.edu>
2015-01-15 03:39:36 -0500

    Rename BitBucket to Bitbucket

--- pages/software.php
+++ pages/software.php
@@ -8,11 +8,11 @@ $template->tab = 'Software';
 
 <h3>MIT Uncertainty Quantification (MUQ) Library</h3>
 <p>MUQ is a collection of tools for constructing models and a collection of UQ-orientated algorithms for working on those model.  Our goal is to provide an easy and clean way to set up and efficiently solve uncertainty quantification problems.</p>
-<p><a class="btn" href="https://bitbucket.org/mituq/muq">Download from BitBucket</a></p>
+<p><a class="btn" href="https://bitbucket.org/mituq/muq">Download from Bitbucket</a></p>
 
 <h3>NOWPAC (Nonlinear Optimization With Path-Augmented Constraints)</h3>
 <p>NOWPAC is a software package for derivative-free nonlinear constrained local optimization. The code is based on a trust region framework using surrogates of minimum Frobenius norm type for the objective function and the constraints. The code does not require gradient information and is designed to work with only black-box evaluations of the objective function and the constraints. In addition to the optimization procedure, NOWPAC provides a noise detection tool which identifies inaccurate black-box evaluations that might corrupt the optimal result or prevent the optimization procedure from making further progress.</p>
-<p><a class="btn" href="https://bitbucket.org/fmaugust/nowpac">Download from BitBucket</a></p>
+<p><a class="btn" href="https://bitbucket.org/fmaugust/nowpac">Download from Bitbucket</a></p>
 
 
 <h4>Related Publications</h4>
